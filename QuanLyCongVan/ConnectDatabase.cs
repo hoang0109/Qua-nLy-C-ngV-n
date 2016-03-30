141681b2115d1191ad83ff0539e91e401ef75560
hoang0109 <hoang0109@gmail.com>
2016-03-30 09:19:37 +0700

    Khởi tạo giao diện ban đầu và Class kết nối CSDL

---
+++ QuanLyCongVan/ConnectDatabase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+namespace QuanLyCongVan
+{
+    class ConnectDatabase
+    {
+        protected static String _connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Database.accdb;Jet OLEDB:Database Password=CATBK123123;";
+        static OleDbConnection connection;
+        public static void OpenConnection()
+        {
+            try
+            {
+                connection = new OleDbConnection(_connectionString);
+                connection.Open();
+                Console.WriteLine("ket noi thanh cong");
+            }
+            catch
+            {
+                Console.WriteLine("ket noi khong thanh cong");
+                connection.Close();
+            }
+
+        }
+        public static void CloseConnection()
+        {
+            if (connection != null)
+            {
+                connection.Close();
+            }
+        }
+        public static DataTable ExcuQuery(string sql)
+        {
+            OpenConnection();
+            DataTable dt = new DataTable();
+            OleDbCommand command = connection.CreateCommand();
+            command.CommandText = sql;
+            OleDbDataAdapter adapter = new OleDbDataAdapter();
+            adapter.SelectCommand = command;
+            adapter.Fill(dt);
+            CloseConnection();
+            return dt;
+        }
+        public static void ExcuNonQuery(string sql)
+        {
+            OpenConnection();
+            OleDbCommand command = connection.CreateCommand();
+            command.CommandText = sql;
+            command.ExecuteNonQuery();
+            CloseConnection();
+        }
+    }
+}
---
+++ QuanLyCongVan/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace QuanLyCongVan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ribbonControl1 = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            this.ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.ShowCongVanDen = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.ribbonPageGroup2 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ribbonControl1
+            // 
+            this.ribbonControl1.ExpandCollapseItem.Id = 0;
+            this.ribbonControl1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribbonControl1.ExpandCollapseItem});
+            this.ribbonControl1.Location = new System.Drawing.Point(0, 0);
+            this.ribbonControl1.MaxItemId = 1;
+            this.ribbonControl1.Name = "ribbonControl1";
+            this.ribbonControl1.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+            this.ribbonPage1});
+            this.ribbonControl1.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonControlStyle.Office2010;
+            this.ribbonControl1.Size = new System.Drawing.Size(758, 144);
+            // 
+            // ribbonPage1
+            // 
+            this.ribbonPage1.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ShowCongVanDen,
+            this.ribbonPageGroup2});
+            this.ribbonPage1.Name = "ribbonPage1";
+            this.ribbonPage1.Text = "Quản lý Công Văn Đến";
+            // 
+            // ShowCongVanDen
+            // 
+            this.ShowCongVanDen.Name = "ShowCongVanDen";
+            this.ShowCongVanDen.Text = "Công Văn Đến";
+            this.ShowCongVanDen.CaptionButtonClick += new DevExpress.XtraBars.Ribbon.RibbonPageGroupEventHandler(this.ShowCongVanDen_CaptionButtonClick);
+            // 
+            // ribbonPageGroup2
+            // 
+            this.ribbonPageGroup2.Name = "ribbonPageGroup2";
+            this.ribbonPageGroup2.Text = "Công Văn Đến";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(651, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AllowFormGlass = DevExpress.Utils.DefaultBoolean.False;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(758, 360);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ribbonControl1);
+            this.Name = "Form1";
+            this.Ribbon = this.ribbonControl1;
+            this.Text = "Phần Mềm Quản Lý Công Văn";
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.Ribbon.RibbonControl ribbonControl1;
+        private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage1;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ShowCongVanDen;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
